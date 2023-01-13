64ecb91ea2078a6a4e11c900d4babd68d5bb3bbe
kaewtar <kaewtarcmu@gmail.com>
2023-01-13 16:34:36 +0700

    done

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,30 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N ,double B[])
+{
+    double x=0 , y=0 , z=1 , a=0, n = A[0] , m = A[0];
+     for(int i=0; i < N ; i++)
+    {
+        x += A[i];
+    }
+    B[0] = x / N ;
+    
+    for(int i=0; i < N ; i++)
+    {
+        y += pow(A[i]-B[0],2);
+        z *= A[i];
+        a += 1.0/A[i];
+        if(A[i] >= m) m = A[i];
+        if(A[i] <= n) n = A[i];
+    }
+    B[1] = sqrt(y/N);
+    B[2] = pow(z,1.0/N);
+    B[3] = N/a;
+    B[4] = m;
+    B[5] = n;
+
+
+   
+}
